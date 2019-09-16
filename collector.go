2a5a62dd285723dfd610f7f55f10102d41514137
Albert Autin <albert.autin@thetradedesk.com>
2019-09-16 09:31:23 -0600

    ver bump

--- collector.go
+++ collector.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-var buildVersion = "0.1.24"
+var buildVersion = "0.1.25"
 
 var (
 	listenPort           = flag.String("listenPort", ":9634", "listen address for prometheus")
