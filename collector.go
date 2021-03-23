47191d4289f80bd11da73c3c1dd91d684c87ccd1
albert.autin <albert.autin@thetradedesk.com>
2021-03-23 15:21:13 -0600

    Minor ver bump

--- collector.go
+++ collector.go
@@ -12,7 +12,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var buildVersion = "0.2.4"
+var buildVersion = "0.2.5"
 var expirationTTLCounts *prometheus.GaugeVec
 var expirationTTLPercents *prometheus.GaugeVec
 
