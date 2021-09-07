2d23294eda301d63d8b3ea994358f37c918689ba
albert.autin <albert.autin@thetradedesk.com>
2021-09-07 14:25:53 -0600

    instant regret on trying to many connections, actually breaking 1024 soft nofile limit. lowering this.

--- collector.go
+++ collector.go
@@ -11,7 +11,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var buildVersion = "2.0.2"
+var buildVersion = "2.0.3"
 var expirationTTLCounts *prometheus.GaugeVec
 var expirationTTLPercents *prometheus.GaugeVec
 
